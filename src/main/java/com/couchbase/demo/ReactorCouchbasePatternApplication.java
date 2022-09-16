e5c5098d90046f9fec01b56a187accdcf73f772f
Laurent Doguin <laurent.doguin@gmail.com>
2022-09-16 15:31:04 +0200

    add spring starter kit

---
+++ src/main/java/com/couchbase/demo/ReactorCouchbasePatternApplication.java
@@ -0,0 +1,13 @@
+package com.couchbase.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorCouchbasePatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorCouchbasePatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/couchbase/demo/ReactorCouchbasePatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couchbase.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorCouchbasePatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
